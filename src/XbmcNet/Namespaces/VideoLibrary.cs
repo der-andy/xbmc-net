e5b8e33632519a0099d292342f12c0bc26139e73
Andreas Bongardt <github@qqxx.de>
2013-02-17 21:05:09 +0100

    added GetMovieDetails function

--- src/XbmcNet/Namespaces/VideoLibrary.cs
+++ src/XbmcNet/Namespaces/VideoLibrary.cs
@@ -10,16 +10,19 @@ using XbmcNet.Schema.Video.Details;
 
 namespace XbmcNet.Namespaces
 {
+    /// <summary>
+    ///     Provides access to the functions of the XBMC VideoLibrary namespace.
+    /// </summary>
     public class VideoLibrary : XbmcNamespace
     {
-        /// <summary>
-        ///     Provides access to the functions of the XBMC VideoLibrary namespace.
-        /// </summary>
-        public VideoLibrary(XbmcConnection xbmc)
+        internal VideoLibrary(XbmcConnection xbmc)
             : base(xbmc)
         {
         }
 
+        /// <summary>
+        ///     5.12.9 Retrieve all movies
+        /// </summary>
         [PublicAPI]
         public async Task<Movie[]> GetMovies(int start = 0, int end = -1)
         {
@@ -36,6 +39,32 @@ namespace XbmcNet.Namespaces
             GetMoviesResult result = await Xbmc.SendRequest<GetMoviesResult>(request);
 
             return result.Movies;
+        }
+
+        /// <summary>
+        ///     5.12.6 Retrieve details about a specific movie
+        /// </summary>
+        public async Task<Movie> GetMovieDetails(int movieId)
+        {
+            var request = new RpcRequest("VideoLibrary.GetMovieDetails")
+                              {
+                                  Params = new
+                                               {
+                                                   movieid = movieId,
+                                                   properties = Schema.Video.Fields.Movie
+                                               }
+                              };
+
+            GetMovieDetailsResult result = await Xbmc.SendRequest<GetMovieDetailsResult>(request);
+
+            return result.MovieDetails;
+        }
+
+        [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+        private class GetMovieDetailsResult
+        {
+            [JsonProperty("moviedetails")]
+            public Movie MovieDetails { get; set; }
         }
 
         [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
